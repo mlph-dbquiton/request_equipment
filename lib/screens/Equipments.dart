400d0ecc4b4275551e72646e499a6b18ce57b328
Domerck Quiton <domerckbrigson@gmail.com>
2022-09-07 00:55:15 +0800

    test commit

--- lib/screens/Equipments.dart
+++
@@ -1,8 +0,0 @@
-class Equipment {
-  final String code;
-  final String specs;
-  final String description;
-  final String status;
-
-  Equipment(this.code, this.specs, this.description, this.status);
-}
--- lib/screens/FormDetails.dart
+++
@@ -1,8 +0,0 @@
-class Form {
-  final String name;
-  final String division;
-  final String purpose;
-  final DateTime schedule;
-
-  Form(this.name, this.division, this.purpose, this.schedule);
-}
